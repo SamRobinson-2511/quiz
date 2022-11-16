62618e87e26621b77651320a06c321d9759aa978
+ <rodarte_sarah@yahoo.com>
2022-11-15 21:52:05 -0700

    updated work

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const ansContainer = document.querySelector("body > div:nth-child(2)")
 const cardQuestion = document.querySelector("body > div:nth-child(3)")
 
 function renderCards(cards){
+    console.log(cards)
     for(i=0;i<cards.length;i++){
+        let div = document.createElement("div")
+        div.id = 'questionCard'
+
+        
         
         //categories: into a dropdown 
         let categories = (cards[i]["category"])
@@ -18,32 +23,51 @@ function renderCards(cards){
         catAnchors.textContent = categories
         catContainer.append(catAnchors)
 
+         //questions: h2 on card
+         let questions = (cards[i]["question"])
+         let questionH2 = document.createElement('h2')
+         div.append(questions)
+
         //answers: correct answers onto buttons 
         let correctAnswers = (cards[i]["correctAnswer"])
         let buttonCorrect = document.createElement('button')
         buttonCorrect.id = 'correct'
         buttonCorrect.classList.add('button')
         buttonCorrect.textContent = correctAnswers
-        ansContainer.append(buttonCorrect)
+        div.append(buttonCorrect)
         
-        //questions: h2 on card
-        let questions = (cards[i]["question"])
-        let questionH2 = document.createElement('h2')
-        cardQuestion.append(questions)
+        buttonCorrect.addEventListener ('click', function() {
+            buttonCorrect.classList.add('correctChosen')
+        })
         
         //difficulty 
         let difficulty = (cards[i]["difficulty"])
         let difficultyLi = document.createElement('li')
         console.log(difficulty)
+        
+        // Incorrect answers
+        let incorrectAnswers = (cards[i]["incorrectAnswers"])
+        for(j=0;j<incorrectAnswers.length;j++){
+            let incorrectAnswer = incorrectAnswers[j] 
+            console.log(incorrectAnswer)
+            let buttonWrong = document.createElement('button')
+            buttonWrong.id = 'wrong'
+            buttonWrong.classList.add('button')
+            buttonWrong.textContent = incorrectAnswer
+            div.append(buttonWrong)
 
-        // console.log(cards[i]["question"])
-//wrong answers only!
-        // for(j=0; j<incorrectAnswers.length; j++){
-        //     let incorrectAnswers = (cards[i]["incorrectAnswers"][j])
-        //     console.log(incorrectAnswers)
-        // }
+            buttonWrong.addEventListener('click', function() {
+                buttonWrong.classList.add('incorrectChosen')
+            })
+        }
+        console.log(incorrectAnswers.length)
+        catContainer.append(div)
     }
+  
 }
+
+
+
 
 // const testBtnDiv = document.createElement('div')
 
